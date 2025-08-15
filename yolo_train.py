a509026ed8ff100cb08d9976a6cf0188fcbf2377
itsMohitShah <mohit.shah165@gmail.com>
2025-08-16 01:37:03 +0200

    Refactor training script to integrate Weights & Biases for better tracking and model checkpointing

--- yolo_train.py
+++ yolo_train.py
@@ -1,15 +1,21 @@
 from ultralytics import YOLO
 from ultralytics import settings
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
 
 if __name__ == "__main__":
-    model = YOLO("yolo11n.pt")
     dataset_yaml = r"dataset\roboflow\football-players-detection.v19-yolo11m.yolov11\data.yaml"
     settings.update({"runs_dir": "runs",
-                     "datasets_dir": "dataset",
+                     "datasets_dir": "datasets",
                      "weights_dir": "weights"})
-    results = model.train(data = dataset_yaml,
-                        epochs = 100,
-                        imgsz = 640,
-                        workers = 1)
+    with wandb.init(project="football-player-tracking", 
+                    job_type="train") as run:
+        model = YOLO("yolo11n.pt")
+        add_wandb_callback(model,enable_model_checkpointing=True)
+        results = model.train(project = "football-player-tracking",
+                            data = dataset_yaml,
+                            epochs = 100,
+                            imgsz = 640,
+                            workers = 1)
 
 
