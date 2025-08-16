a8d1d68ef1468100f02aa4f88b6783ce93150e0d
itsMohitShah <mohit.shah165@gmail.com>
2025-08-16 03:44:16 +0200

    Add inference script for YOLO model with Weights & Biases integration

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,18 @@
 from ultralytics import YOLO
-
+import os
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
 if __name__ == "__main__":
-    weights_path = r"weights/trial1/best.pt"
-    model = YOLO(weights_path)
-    model.predict(source=r"dataset/fromLigen/testvideo.mp4",  
-                  save=True)
+    with wandb.init(project="football-player-tracking", job_type="inference") as run:
+        weights_path = r"weights\002_epochs100\best.pt"
+        test_name = weights_path.split(os.sep)[-2]+"-"
+        model = YOLO(weights_path)
+        add_wandb_callback(model)
+        model.predict(source=r"dataset/fromLigen/testvideo.mp4",  
+                    name = test_name,
+                    save=True, 
+                    save_txt=True,
+                    save_conf=True,
+                    show_labels = False,
+                    show_conf = False,
+                    show = True)
